4b1e51acc53444749f520a882ca245f0510d8ee6
rxg-lm <gadigerajashekar1997@gmail.com>
2026-03-14 10:58:29 +0530

    profile activation added

--- src/main/desi-bazar-ui/src/environments/environment.production.ts
+++ src/main/desi-bazar-ui/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://35.237.230.170:8080/api/v1',  // Your STATIC IP
+    apiUrl: 'http://prod:8080/api/v1',  // Your STATIC IP
     appName: 'Desi Bazar Prod'
 };
--- src/main/desi-bazar-ui/src/environments/environment.ts
+++ src/main/desi-bazar-ui/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8080/api/v1',  // Local Spring Boot
-    appName: 'Desi Bazar Dev'
+    appName: 'Desi Bazar Dev',
+    profile: 'local'
 };
